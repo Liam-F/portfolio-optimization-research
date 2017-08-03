c651fdc73b4a5934f4d560cd1e3d32b949842f17
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2017-08-03 17:12:49 +0200

    Add functions for input and output creation.

--- src/quantopian/main.py
+++ src/quantopian/main.py
@@ -10,6 +10,22 @@ def filter_pairs(pairs):
     )
 
 
+def create_inputs(pairs, features_list):
+    nb_strategies = pairs.shape[1]
+    X = np.zeros((nb_strategies, len(features_list)))
+    for i, strategy in enumerate(pairs.columns):
+        X[i] = pr.compute_features(pairs['2013':'2014'][strategy], features_list)
+    return X
+
+
+def create_outputs(pairs, feature_list):
+    nb_strategies = pairs.shape[1]
+    y = np.zeros((nb_strategies, 1))
+    for i, strategy in enumerate(pairs.columns):
+        y[i] = np.array([ft.sharpe_ratio(pairs['2015'])])
+    return y
+
+
 def main():
     pairs = pd.read_csv('./data/2017-08-03-filtered-in-sample-pairs.csv', parse_dates=True, index_col=0)
 
@@ -18,15 +34,9 @@ def main():
                      ft.sharpe_std, ft.sortino_ratio, ft.drawdown_area, ft.max_drawdown, ft.calmar_ratio,
                      ft.tail_ratio, ft.common_sense_ratio)
 
-    nb_strategies = pairs.shape[1]
+    X = create_inputs(pairs, features_list)
 
-    X = np.zeros((nb_strategies, len(features_list)))
-    for i, strategy in enumerate(pairs.columns):
-        X[i] = pr.compute_features(pairs['2013':'2014'][strategy], features_list)
-
-    y = np.zeros((nb_strategies, 1))
-    for i, strategy in enumerate(pairs.columns):
-        y[i] = np.array([ft.sharpe_ratio(pairs['2015'])])
+    y = create_outputs(pairs, features_list)
 
 
 
