49322a64b7f4edb1714109c4ffc48b809397c2df
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2017-08-07 14:58:25 +0200

    Cleanup of the code.

--- src/quantopian/main.py
+++ src/quantopian/main.py
@@ -1,4 +1,3 @@
-import xgboost
 import numpy as np
 import pandas as pd
 import preprocessing as pr
@@ -11,7 +10,6 @@ from sklearn.preprocessing import robust_scale
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, f1_score, confusion_matrix
 from sklearn.preprocessing import binarize
-
 
 pd.set_option('display.width', 200)
 
@@ -30,13 +28,12 @@ def create_inputs(pairs, features_list):
     return X
 
 
-def create_outputs(pairs, feature_list):
+def create_outputs(pairs):
     nb_strategies = pairs.shape[1]
     y = np.zeros((nb_strategies, 1))
     for i, strategy in enumerate(pairs.columns):
         y[i] = np.array([ft.sharpe_ratio(pairs['2015':'2015'][strategy])])
     return y
-
 
 
 def plot_confusion_matrix(cm, classes,
@@ -75,79 +72,13 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
 
 
-def main():
-    features_list = (ft.trading_days, ft.sharpe_ratio, ft.sharpe_ratio_last_year, ft.annret, ft.annvol,
-                     ft.skewness, ft.kurtosis, ft.information_ratio,
-                     ft.sharpe_std, ft.sortino_ratio, ft.drawdown_area, ft.max_drawdown, ft.calmar_ratio,
-                     ft.tail_ratio, ft.common_sense_ratio)
-
-    pairs = pd.read_csv('./data/2017-08-03-filtered-in-sample-pairs.csv', parse_dates=True, index_col=0)
-
-    if not os.path.exists('./data/2017-08-03-filtered-pairs-features.csv'):
-        print('File does not exist, creating and saving')
-        # pairs = pairs[pairs.columns[:1000]]
-
-        X = create_inputs(pairs, features_list)
-        y = create_outputs(pairs, features_list)
-
-        observations = pd.DataFrame(
-            data=np.hstack([X, y]),
-            columns=[f.__name__ for f in features_list] + ['OUTPUT'],
-            index=pairs.columns
-        ).dropna()
-        observations = observations[np.all(np.isfinite(observations), axis=1)]
-
-        observations_scaled = pd.DataFrame(
-            data=robust_scale(observations),
-            columns=observations.columns,
-            index=observations.index
-        )
-
-        observations_scaled.to_csv('./data/2017-08-03-filtered-pairs-features.csv')
-    else:
-        print('File exists!')
-        observations_scaled = pd.read_csv('./data/2017-08-03-filtered-pairs-features.csv', index_col=0)
-
-    # Throw out observations which are more than 4 standard deviations away from the mean
-    # Note: doesn't really matter for Random Forests
-    observations_scaled = observations_scaled[np.all(observations_scaled.abs() <= 4, axis=1)]
-
-    # sns.pairplot(observations_scaled)
-    # plt.savefig('pairplot.png', bbox_inches='tight')
-
-    X = observations_scaled.drop(['OUTPUT'], axis=1)
-    y = observations_scaled['OUTPUT']
-    groups = np.array([-9999, -1, 0, 0.5, 0.75, 1, 9999])
-    labels = np.arange(groups.shape[0] - 1)
-    y = pd.cut(y, groups, labels=labels)
-
-    X_tr, X_te, y_tr, y_te = train_test_split(X, y, stratify=y, test_size=0.25, random_state=42)
-
-    forest = RandomForestClassifier(n_estimators=100, random_state=43)
-    forest.fit(X_tr, y_tr)
-
-    test_predictions = forest.predict(X_te)
-
-    bin_preds = test_predictions
-    bin_y_te = y_te
-    print(f'F1 score: {f1_score(bin_y_te, bin_preds, average="macro")}')
-
-    cm = confusion_matrix(bin_y_te, bin_preds)
-    names = ['(%s, %s]' % (a, b) for a, b in zip(groups[:-1], groups[1:])]
-    confusion = pd.DataFrame(cm, index=names, columns=names)
-
-    print('Confusion Matrix')
-    print(confusion)
-    plot_confusion_matrix(cm, names)
-
-    # Useful for comparisons when debugging
-    result = y_te.to_frame('true').merge(pd.Series(test_predictions, index=y_te.index, name='pred').to_frame(),
-                                         left_index=True, right_index=True)
-
+def plot_feature_importance(features_list, forest):
+    """
+    This feature plots the features of a random forest ordered by their importance
+    """
     importances = forest.feature_importances_
     std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
     features_names = [features_list[i].__name__ for i in range(len(importances))]
-
     feature_importances = pd.Series(importances, features_names).rename(index={
         'annret': 'Annualized Return',
         'annvol': 'Annualized Volatility',
@@ -165,12 +96,83 @@ def main():
         'trading_days': 'Trading Days (PnL != 0)',
         'kurtosis': 'Kurtosis'
     })
-
     plt.figure()
     feature_importances.sort_values().plot(kind='barh', xerr=std, title='Feature Importance')
     plt.tight_layout()
 
+
+def classification_forest(X, y, features_list):
+    groups = np.array([-9999, -1, 0, 0.5, 0.75, 1, 9999])
+    labels = np.arange(groups.shape[0] - 1)
+    y = pd.cut(y, groups, labels=labels)
+
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, stratify=y, test_size=0.25, random_state=42)
+
+    forest = RandomForestClassifier(n_estimators=100, random_state=43)
+    forest.fit(X_tr, y_tr)
+
+    test_predictions = forest.predict(X_te)
+    bin_preds = test_predictions
+    bin_y_te = y_te
+
+    print(f'F1 score: {f1_score(bin_y_te, bin_preds, average="macro")}')
+
+    cm = confusion_matrix(bin_y_te, bin_preds)
+    names = ['(%s, %s]' % (a, b) for a, b in zip(groups[:-1], groups[1:])]
+    confusion = pd.DataFrame(cm, index=names, columns=names)
+    print('Confusion Matrix')
+    print(confusion)
+    plot_confusion_matrix(cm, names)
+    # Useful for comparisons when debugging
+    result = y_te.to_frame('true').merge(pd.Series(test_predictions, index=y_te.index, name='pred').to_frame(),
+                                         left_index=True, right_index=True)
+
+    plot_feature_importance(features_list, forest)
     plt.show()
+
+
+def main():
+    features_list = (ft.trading_days, ft.sharpe_ratio, ft.sharpe_ratio_last_year, ft.annret, ft.annvol,
+                     ft.skewness, ft.kurtosis, ft.information_ratio,
+                     ft.sharpe_std, ft.sortino_ratio, ft.drawdown_area, ft.max_drawdown, ft.calmar_ratio,
+                     ft.tail_ratio, ft.common_sense_ratio)
+
+    features_csv = './data/2017-08-07-filtered-pairs-features.csv'
+
+    if not os.path.exists(features_csv):
+        print('File does not exist, creating and saving')
+        pairs = pd.read_csv('./data/2017-08-03-filtered-in-sample-pairs.csv', parse_dates=True, index_col=0)
+
+        X = create_inputs(pairs, features_list)
+        y = create_outputs(pairs)
+
+        observations = pd.DataFrame(
+            data=np.hstack([X, y]),
+            columns=[f.__name__ for f in features_list] + ['OUTPUT'],
+            index=pairs.columns
+        ).dropna()
+        observations = observations[np.all(np.isfinite(observations), axis=1)]
+
+        observations_scaled = pd.DataFrame(
+            data=robust_scale(observations),
+            columns=observations.columns,
+            index=observations.index
+        )
+
+        observations_scaled.to_csv(features_csv)
+    else:
+        print('File exists!')
+        observations_scaled = pd.read_csv(features_csv, index_col=0)
+
+    # Throw out observations which are more than 4 standard deviations away from the mean
+    # Note: doesn't really matter for Random Forests
+    observations_scaled = observations_scaled[np.all(observations_scaled.abs() <= 4, axis=1)]
+
+    X = observations_scaled.drop(['OUTPUT'], axis=1)
+    y = observations_scaled['OUTPUT']
+
+    classification_forest(X, y, features_list)
+
 
 if __name__ == '__main__':
     main()
